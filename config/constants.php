d9d68d2ea7414319a762c9dce371e69c07f613d7
jaymyhealth <jaymoh53@gmail.com>
2022-09-06 17:40:25 +0300

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,9 +1,9 @@
 <?php
 
-define('MERCHANT_ID', '');
-define('PROFILE_ID', '');
-define('ACCESS_KEY', '');
-define('SECRET_KEY', '');
+define('MERCHANT_ID', 'ecomr005194');
+define('PROFILE_ID', 'A1756314-A443-43D0-B212-56B3649D4306');
+define('ACCESS_KEY', 'fd449bba4a313a058863a6fe55eb842d');
+define('SECRET_KEY', '28369e489dfd4fc7b641693c2aada8300c403ea97b294617a3200f431615849ea912c45480ca48d18371fd5df105afd0c9f2b6413dc54a09b94e540bc7ab4e5e07fc23774b9d4dcba7468d579314e2104ad155e3503442e8a8b46d7aa82df421e0afcf7be0b348fd9192794bfbd167eac77c1374d79842fc9ccbe578ec86015b');
 define('HMAC_SHA256', 'sha256');
 
 define('CYBS_BASE_URL', 'https://secureacceptance.cybersource.com');
